653c6b70ab9edeca9f3bee0f81838fc739fef192
Edward Colley <edward.colley@outlook.com>
2019-11-06 12:51:20 -0800

    added navigation header passed params to new recipe screen

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import { useScreens } from 'react-native-screens';
+
+import MealsNavigator from './navigation/MealsNavigator';
+
+useScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator />
   );
 }
 
@@ -16,4 +39,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  // text: {
+  //   fontFamily: 'open-sans'
+  // },
 });
---
+++ screens/CategoryMealScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, Platform } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
+
+const CategoryMealScreen = props => {
+    const catId = props.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+    return (
+        <View style={styles.screen}>
+            <Text>The Category Meal Screen!</Text>
+            <Text>{selectedCategory.title}</Text>
+            <Button title="Go to Details!" onPress={() => {
+                props.navigation.navigate({ routeName: 'MealDetail' })
+            }} />
+        </View>
+    )
+};
+
+CategoryMealScreen.navigationOptions = (navigationData) => {
+    const catId = navigationData.navigation.getParam('categoryId')
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+
+    return {
+        headerTitle: selectedCategory.title
+    }
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default CategoryMealScreen;
