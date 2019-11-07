aa73105ebaf30488a0863b414c97fd2399a5d294
Edward Colley <edward.colley@outlook.com>
2019-11-06 17:01:44 -0800

    cannot get favorite icon to show up

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { Ionicons } from '@expo/vector-icons';
+
+import { MEALS } from '../data/dummy-data';
+import { HeaderButton } from '../components/HeaderButton';
 
 const MealDetailsScreen = props => {
-    return(
+
+    const mealId = props.navigation.getParam('mealId');
+
+    const selectedMeal = MEALS.find(meal => mealId === meal.id)
+
+    return (
         <View style={styles.screen}>
-            <Text>The Meal Details Screen!</Text>
+            <Text>{selectedMeal.title}</Text>
         </View>
     )
+};
+
+MealDetailsScreen.navigationOptions = (navigationData) => {
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => mealId === meal.id)
+
+    return {
+        headerTitle: selectedMeal.title,
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Favorite"
+                    iconName="ios-star" 
+                    onPress={() => { }}
+                />
+            </HeaderButtons>
+        )
+    };
+
 };
 
 const styles = StyleSheet.create({
